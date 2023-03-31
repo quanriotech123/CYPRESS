d9c4620fc5ad066d3b23d544d4828134fd8b32f1
quanriotech123 <quanriotech@gmail.com>
2023-03-31 12:24:31 +0500

    removed it inside

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -5,11 +5,7 @@ describe('template spec', () => {
 
   
   it('passes', () => {
-    cy.visit('http://blockchainmarketplace.quanrio.com/')
-    const ln = new cls();
-    ln.clickloginbtn()
-    ln.enterusername('lkajlkdjlajdlff')
-    ln.enterpassword('khan ali')
+
 
   })
 })
