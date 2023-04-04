84be47cf653fd7a1c8cca44dde54a9db47141cee
quanriotech123 <quanriotech@gmail.com>
2023-04-04 12:47:36 +0500

    khan

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,11 +1,61 @@
 
 import cls from "../POM/login1"
 
+describe('', () => {
 
-describe('template spec', () => {
-
-  
-  it('passes', () => {
-khan
+  it('Click on the login button when all fields remain blank', () => {
+    cy.visit('http://blockchainmarketplace.quanrio.com/') 
+    cy.get('.signUpBtnTxt').click()
+    cy.get('.p-0 > a').click()
+    cy.get('.d-flex > .btn').click()
+    cy.get('#swal2-title').invoke('text').then($errormessage=>{
+      
+     if($errormessage=='Whoops...')
+       {
+        cy.log('Test Pass')
+       }
+     else{
+        cy.log('Test Fail')
+         }
+    })
   })
+  ///////////////////////////////////////////////////////////
+  it('Enter an invalid email && a valid password',()=>{
+    cy.visit('http://blockchainmarketplace.quanrio.com/') 
+    cy.get('.signUpBtnTxt').click()
+    cy.get(':nth-child(1) > .form-control').type('shahjehan@gm')
+    cy.get(':nth-child(2) > .form-control').type('12345')
+    cy.get(':nth-child(2) > .btn').click()
+    cy.get('#swal2-title').invoke('text').then($errormessage=>{
+      
+      if($errormessage=='Whoops...')
+      {
+        cy.log('Test Pass')
+      }
+      else{
+         cy.log('Test Fail')
+      }
 })
+})
+/////////////////////////////////////////////////////////
+it.only('Enter a valid email && an invalid password',()=>{
+  cy.visit('http://blockchainmarketplace.quanrio.com/') 
+  cy.get('.signUpBtnTxt').click()
+  cy.get(':nth-child(1) > .form-control').type('shahjehan@gmail.com')
+  cy.get(':nth-child(2) > .form-control').type('123454545')
+  cy.get(':nth-child(2) > .btn').click()
+  cy.get('#swal2-title').invoke('text').then($errormessage=>{
+    
+    if($errormessage=='Whoops...')
+    {
+      cy.log('Test Pass')
+    }
+    else{
+       cy.log('Test Fail')
+    }
+})
+})
+////////////////////////////////////////////////////////////
+})
+
+
