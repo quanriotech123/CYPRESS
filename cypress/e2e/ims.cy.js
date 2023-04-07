77803f199b215d9ce04aa0c8166cb901cbc4563b
quanriotech123 <quanriotech@gmail.com>
2023-04-07 15:36:25 +0500

    message

--- cypress/e2e/ims.cy.js
+++ cypress/e2e/ims.cy.js
@@ -3,6 +3,6 @@ describe('template spec', () => {
     cy.visit('https://imsciences.edu.pk/')
     cy.get('#menu-item-1893 > .dropdown-togglee').invoke('show').click()
     cy.contains('Core Values').click({force: true})
-    cy.url().should('include','core-values')
+    cy.url().should('include','core-values')     
   })
 })
